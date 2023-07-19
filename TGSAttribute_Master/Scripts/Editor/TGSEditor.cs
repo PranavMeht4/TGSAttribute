8c592b20dab9413173abbbe0f5e941eb9fe7fbd4
Pranav Mehta <pranav0123.pm@gmail.com>
2023-07-19 12:25:23 +0530

    Update TGSEditor.cs

--- TGSAttribute_Master/Scripts/Editor/TGSEditor.cs
+++ TGSAttribute_Master/Scripts/Editor/TGSEditor.cs
@@ -8,37 +8,36 @@ namespace TGSAttributes.Editor
     [CustomEditor(typeof(MonoBehaviour), true)]
     public class TGSEditor : UnityEditor.Editor
     {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-            var mono = target as MonoBehaviour;
+        // public override void OnInspectorGUI()
+        // {
+        // base.OnInspectorGUI();
+        // var mono = target as MonoBehaviour;
 
-            var type = mono.GetType();
-            var flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-            var fields = type.GetFields(flags);
-            foreach (var field in fields)
-            {
-                foreach (Attribute attribute in field.GetCustomAttributes(true))
-                {
-                    if (attribute is IFieldAttribute customAttribute)
-                    {
-                        customAttribute.Handle(mono, field, attribute);
-                    }
-                }
-            }
+        // var type = mono.GetType();
+        // var flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+        // var fields = type.GetFields(flags);
+        // foreach (var field in fields)
+        // {
+        //     foreach (Attribute attribute in field.GetCustomAttributes(true))
+        //     {
+        //         if (attribute is IFieldAttribute customAttribute)
+        //         {
+        //             customAttribute.Handle(mono, field, attribute);
+        //         }
+        //     }
+        // }
 
-            var methods = type.GetMethods(flags);
-            foreach (var method in methods)
-            {
-                foreach (Attribute attribute in method.GetCustomAttributes(true))
-                {
-                    if (attribute is IMethodAttribute customAttribute)
-                    {
-                        customAttribute.Handle(mono, method, attribute);
-                    }
-                }
-            }
-
-        }
+        // var methods = type.GetMethods(flags);
+        // foreach (var method in methods)
+        // {
+        //     foreach (Attribute attribute in method.GetCustomAttributes(true))
+        //     {
+        //         if (attribute is IMethodAttribute customAttribute)
+        //         {
+        //             customAttribute.Handle(mono, method, attribute);
+        //         }
+        //     }
+        // }
+        // }
     }
 }
